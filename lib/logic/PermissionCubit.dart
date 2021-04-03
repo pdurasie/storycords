c174d2b4b8ae3ad0da5fc72f905e713c509f525a
Patrick Durasiewicz <pdurasie@protonmail.com>
2021-04-03 15:50:01 +0200

    Work on cubit

--- lib/logic/PermissionCubit.dart
+++ lib/logic/PermissionCubit.dart
@@ -1,13 +1,24 @@
 import 'package:bloc/bloc.dart';
+import 'package:injectable/injectable.dart';
 
+/*
 enum PermissionState {
-  Requesting,
+  RequestInProgress,
   PermanentlyDenied,
   Denied,
   Restricted,
   Granted,
 }
 
+ */
+
+abstract class PermissionState {
+
+}
+
+@injectable
 class PermissionCubit extends Cubit<PermissionState> {
   PermissionCubit() : super(PermissionState.Denied);
+
+  void setRequesting() => 
 }
